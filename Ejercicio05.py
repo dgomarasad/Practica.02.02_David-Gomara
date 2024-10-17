ffe605d0611907825af9712967e070ee1d917045
sgimenelan <dgomarasad@educacion.navarra.es>
2024-10-17 13:24:45 +0200

    Create Ejercicio05.py

---
+++ Ejercicio05.py
@@ -0,0 +1,4 @@
+#Escribir un programa que pida al usuario que introduzca una frase en la 
+#consola y muestre por pantalla la frase invertida.
+Frase = str(input('Dime una frase '))
+print(Frase[::-1] )
