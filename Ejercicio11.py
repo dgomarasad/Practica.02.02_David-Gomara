70b7a29cde7639a2ffc9c11f0fdeae334a753cd4
dgomarasad <dgomarasad@educacion.navarra.es>
2024-10-18 09:25:26 +0200

    Create Ejercicio11.py

---
+++ Ejercicio11.py
@@ -0,0 +1,10 @@
+#Escribir un programa que pregunte el nombre de un producto, su precio y un
+#número de unidades y muestre por pantalla una cadena con el nombre del 
+#producto seguido de su precio unitario con 6 dígitos enteros y 2 decimales,
+#el número de unidades con tres dígitos y el coste total con 8 dígitos
+#enteros y 2 decimales.
+producto = input('Dime el nombre del producto:')
+precio = float(input('Dime el precio unitario:'))
+unidades = int(input('Numero de unidades:'))
+precio_total = (precio * unidades)
+print('El producto {} vale {:9.2f}€, quiero {:3d} unidades y el coste total es {:11.2f}€'.format( producto, precio, unidades,precio_total))
